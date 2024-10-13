4c69993bfc8b057806a3e757f077d3dc6c953c91
Etien <etienne.espacia@student.hu.nl>
2024-10-13 20:34:55 +0200

    De versie die werkt

--- Menu.py
+++ Menu.py
@@ -1,36 +1,22 @@
-print("Welkom bij PloeqieAppStore")
-
-def selecteer_spel():
+def hoofdmenu():
     while True:
-        keuze = input("Selecteer een spel: Nummerraadspel (1) of Galgje (2): ")
+        print("Welkom bij het PloeqieAppStore! Er volgen nu 3 keuzes die u kunt selecteren om te doen.")
+        print("1. Nummer Raadspel")
+        print("2. Galgje")
+        print("3. Stoppen")
 
-        if keuze in ["Nummerraadspel", "1", "nummerraadspel"]:
-            return "nummerraadspel"
+        keuze = input("Kies een spel: ")
 
-        elif keuze in ["Galgje", "2", "galgje"]:
-            return "galgje"
-
+        if keuze == '1':
+            from spellen import Cijferraadspel
+            Cijferraadspel()
+        elif keuze == '2':
+            from spellen import Galgjespel
+            Galgjespel()
+        elif keuze == '3':
+            print("Bedankt voor het spelen!")
+            break
         else:
-            print("Ongeldige keuze, probeer het opnieuw.")
+            print("Ongeldige keuze. Probeer het opnieuw.")
 
-def nummerraadspel():
-    print("Je speelt nu het Nummerraadspel!")
-    from spellen import cijferraadspel
-
-def galgjespel():
-    print("Je speelt nu Galgje!")
-    from spellen import Galgjespel
-
-while True:
-    starten = selecteer_spel()
-
-    if starten == "nummerraadspel":
-        nummerraadspel()
-
-    elif starten == "galgje":
-        galgjespel()
-
-    opnieuw = input("Wil je een nieuw spel starten? (ja/nee): ").lower()
-    if opnieuw != "ja":
-        print("Bedankt voor het spelen!")
-        break
+hoofdmenu()
--- spellen.py
+++ spellen.py
@@ -1,24 +1,23 @@
-def cijferraadspel():
-    import random
+import random
 
+def Cijferraadspel():
     laagste = 1
-    hoogste = int(input("Geef het hoogste getal waar je mee wilt spelen:  "))
-    max_attempts = int(input("Geef aan hoeveel kansen je wilt hebben:  "))
-
+    hoogste = int(input("Geef het hoogste getal waar je mee wilt spelen: "))
+    max_attempts = int(input("Geef aan hoeveel kansen je wilt hebben: "))
     getal = random.randint(laagste, hoogste)
 
     def get_guess():
         while True:
             try:
-                guess = int(input(f"Raad een getal tusen {laagste} em {hoogste}: "))
+                guess = int(input(f"Raad een getal tussen {laagste} en {hoogste}: "))
                 if laagste <= guess <= hoogste:
                     return guess
                 else:
-                    print("ongeldige input. Voer een getal tussen de waardes in")
+                    print("Ongeldige input. Voer een getal tussen de waardes in.")
             except ValueError:
-                print("Ongeldige invoer probeer het opnieuw.")
+                print("Ongeldige invoer, probeer het opnieuw.")
 
-    def check_guess(guess, secret_number):
+    def check_guess(guess, getal):
         if guess == getal:
             return "Correct"
         elif guess < getal:
@@ -36,143 +35,189 @@ def cijferraadspel():
             result = check_guess(guess, getal)
 
             if result == "Correct":
-                print(f"Gefeliciteerd! Je heb {getal} in {attempts}x geraden.")
+                print(f"Gefeliciteerd! Je hebt {getal} in {attempts}x geraden.")
                 won = True
                 break
             else:
                 print(f"{result}. Probeer opnieuw!")
 
         if not won:
-            print(f"Sorry, je gokbeurten zijn over het juiste getal was {getal}.")
+            print(f"Sorry, je gokbeurten zijn over. Het juiste getal was {getal}.")
 
-    if __name__ == "__main__":
-        print("Welkom bij het getal raad spel!")
-        nummerraadspel()
+    print("Welkom bij het getal raad spel!")
+    nummerraadspel()
 
+    nogpotje = input("Wil je nog een potje spelen? (ja/nee): ").lower()
+    if nogpotje == "ja":
+        Cijferraadspel()
+    else:
+        from Menu import hoofdmenu  # Zorg ervoor dat deze import correct is
+        hoofdmenu()
 
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+#Hier begint het galgje!!
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
+import random
 
+def print_galg(miss_count):
+    stages = [
+        """  
+            --------
+            |      |
+            |      
+            |    
+            |     
+            |     
+            -   
+         """,
+        """  
+            --------
+            |      |
+            |      O
+            |    
+            |     
+            |     
+            -   
+         """,
+        """  
+            --------
+            |      |
+            |      O
+            |      |
+            |     
+            |     
+            -   
+         """,
+        """  
+            --------
+            |      |
+            |      O
+            |     /|
+            |     
+            |     
+            -   
+         """,
+        """  
+            --------
+            |      |
+            |      O
+            |     /|\\
+            |     
+            |     
+            -   
+         """,
+        """  
+            --------
+            |      |
+            |      O
+            |     /|\\
+            |     / 
+            |     
+            -   
+         """,
+        """  
+            --------
+            |      |
+            |      O
+            |     /|\\
+            |     / \\
+            |     
+            -   
+         """,
+    ]
+
+    if miss_count < len(stages):
+        print(stages[miss_count])
+    else:
+        print(stages[-1])
 
 
 def Galgjespel():
-    import random
+    woordenlijst = [
+        'afspraak', 'aanslag', 'abonnement', 'afdeling', 'aflevering', 'afspraak', 'afwas', 'afkomst', 'afkorting',
+    'afspraak', 'afschuw', 'afgunst', 'afschaffen', 'afspraak', 'afstammeling', 'afval', 'afvalbak', 'afvalscheiding',
+    'afvalverwerking', 'afvoer', 'afspelen', 'afspiegeling', 'afspraak', 'afschrikkend', 'afschuwelijk', 'agressief',
+    'ahorn', 'alleen', 'afleiding', 'almond', 'aloe', 'alternatief', 'alpen', 'amber', 'ananas', 'analytisch',
+    'aanpassing', 'ander', 'andere', 'anesthesie', 'angst', 'ankerkruid', 'applaus', 'appel', 'applicatie', 'april',
+    'argeloos', 'aqua', 'aroma', 'armband', 'arrest', 'artiest', 'atlas', 'autobiografie', 'auto', 'bacterie',
+    'ballet', 'banaan', 'barbecue', 'barkeeper', 'barometer', 'beweging', 'beetje', 'belangrijk', 'belastingen',
+    'belgië', 'belgisch', 'belletje', 'beneden', 'bereiding', 'berichten', 'beroepsmatig', 'beslissing', 'betrekking',
+    'beweging', 'bewaking', 'bewustzijn', 'biodiversiteit', 'biografie', 'bizarre', 'bladen', 'bladzijde', 'blessure',
+    'blinde', 'bloem', 'bloemkool', 'boerderij', 'boogschutter', 'boor', 'boom', 'boek', 'boomgaard', 'boot', 'boze',
+    'bron', 'brood', 'broodjes', 'broodrooster', 'brug', 'buik', 'bundel', 'burgt', 'butterfly', 'cabaret', 'cadeau',
+    'camera', 'campus', 'capuchon', 'caravan', 'carrière', 'carnaval', 'ceremonie', 'chaos', 'chocolade', 'cirkel',
+    'clown', 'communicatie', 'computer', 'consultant', 'contract', 'cursus', 'da Vinci', 'dagelijkse', 'dansen',
+    'deksel', 'delfstoffen', 'denkbeeldig', 'denkproces', 'derde', 'desinfecteren', 'dezelfde', 'diamant', 'dieet',
+    'doel', 'doelstellingen', 'dominator', 'donder', 'doorn', 'dubbel', 'duif', 'duizend', 'duizelingwekkend', 'edukatief',
+    'economie', 'educatie', 'effectief', 'efectief', 'elegant', 'embleem', 'empire', 'empirisch', 'engagement', 'enigma',
+    'enquête', 'epidemie', 'erfgoed', 'ergonomie', 'erfgenaam', 'ervaringsdeskundige', 'ervaringsuitwisseling', 'eerste',
+    'eetstoornis', 'egocentrisch', 'eigen', 'eind', 'eisen', 'elektriciteit', 'element', 'elementair', 'elf', 'elektrisch',
+    'emotioneel', 'envelop', 'envelope', 'enthousiast', 'ergonomisch', 'etentje', 'evenement', 'evenwicht', 'fabriek',
+    'faciliteit', 'familie', 'farce', 'fantasie', 'fauna', 'fiets', 'figuur', 'film', 'flap', 'flessen', 'flits',
+    'flitslicht', 'fluit', 'fluiter', 'fotografie', 'frisdrank', 'fruit', 'funtastisch', 'galgje', 'geluid', 'geluksgevoel',
+    'geolied', 'geopolitiek', 'geplande', 'gerucht', 'gezelschap', 'gezondheid', 'gezicht', 'geschiedenis', 'gezin',
+    'glaasje', 'globe', 'goede', 'grond', 'groen', 'grootte', 'haken', 'hals', 'handhaving', 'hapje', 'harigheid',
+    'hardloop', 'hectare', 'hedendaags', 'herkenning', 'herinnering', 'hierarchie', 'historie', 'hoed', 'hoofd', 'hoop',
+    'horloge', 'horizontaal', 'huishoudelijk', 'huis', 'huiskamer', 'huisterrein', 'ijverig', 'ijverig', 'ijverig',
+    'ijskast', 'ijskoude', 'ijzel', 'ijzer', 'ik', 'illustratie', 'immigratie', 'impact', 'instructie', 'internet',
+    'interview', 'introductie', 'invitatie', 'investering', 'io', 'isolatie', 'jaarlijks', 'januari', 'jargon', 'jeugd',
+    'jodendom', 'jong', 'junior', 'juweel', 'kader', 'kaas', 'kaars', 'kaart', 'kader', 'kantoor', 'kaos', 'kerst',
+    'keuken', 'kijken', 'klank', 'klankkast', 'klapper', 'klas', 'kleed', 'klein', 'kleinkind', 'knap', 'koppeling',
+    'kopen', 'kras', 'krant', 'kruis', 'kruid', 'kruidenierswinkel', 'kruiswoord', 'laagland', 'laptop', 'lasagne',
+    'lastig', 'levensonderhoud', 'levensstijl', 'licht', 'lichaam', 'lid', 'lingerie', 'lokaal', 'luchthaven', 'luidruchtig',
+    'luifel', 'maak', 'maken', 'maaltijd', 'magie', 'magnetron', 'management', 'manier', 'medewerker', 'medicijn',
+    'melk', 'menu', 'meten', 'metropolis', 'milieu', 'minister', 'moeder', 'moeilijk', 'moment', 'motor', 'museum',
+    'mysterie', 'mystiek', 'mysterieuze', 'naald', 'naamsvermelding', 'naamwoord', 'neef', 'negen', 'netwerk', 'nieuw',
+    'nieuws', 'nomineren', 'omgang', 'omroep', 'ontbijt', 'ontdekken', 'ontdoen', 'ontspanning', 'opdracht', 'opkomst',
+    'oplossing', 'opmerking', 'organisch', 'organisatie', 'overeenkomst', 'overlijden', 'overzien', 'overwinning',
+    'overtuiging', 'paddle', 'panda', 'parachute', 'park', 'partner', 'pech', 'peper', 'pesto', 'piano', 'pijpen',
+    'pilaar', 'pincet', 'pizza', 'plan', 'plank', 'plankton', 'plezier', 'plezier', 'pluim', 'pluimvee', 'pocket',
+    'politiek', 'polyester', 'pool', 'pop', 'poëzie', 'portfolio', 'procent', 'proef', 'profiel', 'project', 'pronk',
+    'publiciteit', 'puin', 'publiek', 'quarantaine', 'quilt', 'raaf', 'raam', 'raar', 'raisin', 'rank', 'reageren',
+    'recept', 'redacteur', 'relatie', 'remmen', 'repetitie', 'revolutie', 'rijden', 'rijken', 'rolstoel', 'romantisch',
+    'ronde', 'rook', 'rooster', 'rustig', 'rustplaats', 'schaap', 'schaduw', 'schakelaar', 'scheiding', 'schijf',
+    'schilder', 'schimmel', 'school', 'schoon', 'schrijf', 'schrijver', 'scooter', 'sneak', 'sneeuw', 'snijden', 'soep',
+    'soul', 'spiegel', 'sport', 'sprinkhaan', 'standaard', 'standpunt', 'steun', 'stijl', 'stolperen', 'stoel', 'stoet',
+    'stop', 'stoplicht', 'stroming', 'smaak', 'sneeuwbal', 'sneeuwman', 'sneeuwvlok', 'sneeuwstorm', 'snel', 'spanning',
+    'spelen', 'spelt', 'spider', 'spons', 'spreeuw', 'stad', 'stap', 'star', 'streep', 'stijl', 'stijg',
+    ]
 
-    def print_galg(miss_count):
-        stages = [
-            """  
-                --------
-                |      |
-                |      
-                |    
-                |     
-                |     
-                -   
-             """,
-            """  
-                --------
-                |      |
-                |      O
-                |    
-                |     
-                |     
-                -   
-             """,
-            """  
-                --------
-                |      |
-                |      O
-                |      |
-                |     
-                |     
-                -   
-             """,
-            """  
-                --------
-                |      |
-                |      O
-                |     /|
-                |     
-                |     
-                -   
-             """,
-            """  
-                --------
-                |      |
-                |      O
-                |     /|\\
-                |     
-                |     
-                -   
-             """,
-            """  
-                --------
-                |      |
-                |      O
-                |     /|\\
-                |     / 
-                |     
-                -   
-             """,
-            """  
-                --------
-                |      |
-                |      O
-                |     /|\\
-                |     / \\
-                |     
-                -   
-             """,
-        ]
+    te_raden_woord = random.choice(woordenlijst)
+    te_raden_letters = set(te_raden_woord)
+    geraden_letters = set()
+    foutieve_beurten = 0
+    max_fouten = int(input("Geef aan hoeveel kansen je wilt hebben:  "))
 
-        # Zorg ervoor dat je niet verder gaat dan het aantal stages
-        if miss_count < len(stages):
-            print(stages[miss_count])
+    print("Welkom bij Galgje!")
+
+    while len(te_raden_letters) > 0 and foutieve_beurten < max_fouten:
+        print(f"Je hebt {max_fouten - foutieve_beurten} pogingen over.")
+        print("Geraadde letters: ", " ".join(geraden_letters))
+        print("Huidige status: ", " ".join([letter if letter in geraden_letters else '_' for letter in te_raden_woord]))
+
+        gok = input("Raad een letter: ").lower()
+
+        if gok in geraden_letters:
+            print("Je hebt deze letter al geraden.")
+        elif gok in te_raden_letters:
+            geraden_letters.add(gok)
+            te_raden_letters.remove(gok)
+            print(f"Goed gedaan! {gok} zit in het woord.")
         else:
-            print(stages[-1])  # Laat de laatste galg zien als je meer foutieve beurten hebt
+            foutieve_beurten += 1
+            print(f"Fout! {gok} zit niet in het woord.")
+            print_galg(foutieve_beurten)
 
-    def galgje():
-        woordenlijst = [
-            'aan', 'aanbod', 'aanraken', 'aanval', 'aap', 'aardappel',
-            # Voeg hier meer woorden toe als nodig
-        ]
+    if foutieve_beurten == max_fouten:
+        print(f"Helaas, je hebt verloren. Het woord was {te_raden_woord}.")
+    else:
+        print(f"Gefeliciteerd! Je hebt het woord {te_raden_woord} geraden.")
 
-        te_raden_woord = random.choice(woordenlijst)
-        te_raden_letters = set(te_raden_woord)
-        geraden_letters = set()
-        foutieve_beurten = 0
-        max_fouten = 6  # Maximaal aantal mislukte beurten voor een spel
+    nogpotje = input("Wil je nog een potje spelen? (ja/nee): ").lower()
 
-        print("Welkom bij Galgje!")
+    if nogpotje == "ja":
+        Galgjespel()
+    else:
+        from Menu import selecteer_spel
 
-        while len(te_raden_letters) > 0 and foutieve_beurten < max_fouten:
-            print(f"Je hebt {max_fouten - foutieve_beurten} pogingen over.")
-            print("Geraadde letters: ", " ".join(geraden_letters))
-            print("Huidige status: ",
-                  " ".join([letter if letter in geraden_letters else '_' for letter in te_raden_woord]))
-
-            gok = input("Raad een letter: ").lower()
-
-            if gok in geraden_letters:
-                print("Je hebt deze letter al geraden.")
-            elif gok in te_raden_letters:
-                geraden_letters.add(gok)
-                te_raden_letters.remove(gok)
-                print(f"Goed gedaan! {gok} zit in het woord.")
-            else:
-                foutieve_beurten += 1
-                print(f"Fout! {gok} zit niet in het woord.")
-                print_galg(foutieve_beurten)
-
-        if foutieve_beurten == max_fouten:
-            print(f"Helaas, je hebt verloren. Het woord was {te_raden_woord}.")
-        else:
-            print(f"Gefeliciteerd! Je hebt het woord {te_raden_woord} geraden.")
-
-        nogpotje = input("Wil je nog een potje spelen?").lower()
-
-        if nogpotje == "ja":
-            galgje()
-        else:
-            from Menu import selecteer_spel
-
-    galgje()
