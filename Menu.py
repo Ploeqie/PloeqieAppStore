e57dae0ef2e39d596d7d1b14faca9d89e92dc951
Etien <etienne.espacia@student.hu.nl>
2024-10-13 19:32:36 +0200

    Test

---
+++ Menu.py
@@ -0,0 +1,6 @@
+print("Welkom bij PloeqieAppStore")
+
+Keuze = input(f"Hieronder zie je de games \n1. Getalraadspel \n2. Galgje \nKies een spel dat je wilt spelen: ")
+
+if keuze == "1"
+    print("het werkt")
